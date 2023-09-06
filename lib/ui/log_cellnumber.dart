8d7778a66a8a4b24d83d261dbf026b245f138539
Enzo <enzo.rva@gmail.com>
2023-09-06 17:37:10 -0300

    validation and auto formating for cellphone number

--- lib/ui/log_cellnumber.dart
+++ lib/ui/log_cellnumber.dart
@@ -10,11 +10,37 @@ class LogCell extends StatefulWidget {
 }
 
 class _LogCellState extends State<LogCell> {
+  RegExp regExp = RegExp(r'^\(?[1-9]{2}\)?\s?9?[6789][0-9]{3,4}\-?[0-9]{4}$');
+  TextEditingController phoneNumberController = TextEditingController();
+
+  @override
+  void dispose() {
+    phoneNumberController.dispose();
+    super.dispose();
+  }
+
+  void updateFormattedPhoneNumber(String text) {
+    String formatted = text.replaceAllMapped(
+      RegExp(r'(\d{2})(\d{5})(\d{4})'),
+      (Match m) => "(${m[1]}) ${m[2]}-${m[3]}",
+    );
+
+    phoneNumberController.value = phoneNumberController.value.copyWith(
+      text: formatted,
+      selection: TextSelection.collapsed(offset: formatted.length),
+    );
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: formKeyCell,
       child: TextFormField(
+        controller: phoneNumberController,
+        keyboardType: TextInputType.phone,
+        autocorrect: false,
         decoration: InputDecoration(
             focusedBorder: const OutlineInputBorder(
               borderSide: BorderSide(color: Color(0xffC1D6FB), width: 4),
@@ -31,8 +57,13 @@ class _LogCellState extends State<LogCell> {
         validator: (value) {
           if (value == null || value.isEmpty) {
             return 'Please enter some text';
+          } else if (!regExp.hasMatch(value)) {
+            return 'Please enter valid cellphone number';
           }
           return null;
+        },
+        onChanged: (value) {
+          updateFormattedPhoneNumber(value);
         },
       ),
     );
